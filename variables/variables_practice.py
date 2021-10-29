84abd6a7e1a10aab2b33920b5efbae33cdaab377
Julian g <90346616+XDTRASHXD@users.noreply.github.com>
2021-10-29 14:50:41 -0400

    {4a} v0.2
    printed lives_remaining, forty_time, and death_cry

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,1 +1,12 @@
-# Variables practice, Julian Guevara. 10/29/21 2:22pm, 0.0
+# Variables practice, Julian Guevara. 10/29/21 2:50pm, 0.2
+
+# Three Common Types of Data
+
+lives_remaining = 13 #INTEGERS, any +/- WHOLE number and 0.
+forty_time =4.21 # FLOATING POINT, any +/- number with a decimal
+death_cry = "Curse you, zagreus! I cannot stand you!"
+#Line 7 is a STRING --0 collection of letters/numbers/other chars
+
+print(lives_remaining)
+print(forty_time)
+print(death_cry)
