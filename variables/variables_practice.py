407eb80bfdf3d3a2777d28a66984c1a30358cbe5
Julian g <90346616+XDTRASHXD@users.noreply.github.com>
2021-10-29 14:41:32 -0400

    Revert "Update variables_practice.py"
    This reverts commit 6657e5bc4f3e83fd650e6f87a55065237a63facd.

--- variables/variables_practice.py
+++ variables/variables_practice.py
@@ -1,8 +1,1 @@
-# Variables practice, Julian Guevara, 10/29/21 2:37pm, 0.1
-
-# Three Common Types of Data
-
-lives_remaining = 5 # INTEGERS, any +/- WHOLE number and 0
-forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal
-death_cry = "Curse you, zagreus! I cannot stand you!"
-# Line 7 is a STRING --0 collection of letters/numbers/other chars
+# Variables practice, Julian Guevara. 10/29/21 2:22pm, 0.0
